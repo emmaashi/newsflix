a3635185698b1ee631d14079d23d5ea8510631b2
Samantha Mac <70736045+samanthavmac@users.noreply.github.com>
2024-08-03 13:45:42 -0400

    Updated data.py

--- server/data.py
+++ server/data.py
@@ -1,32 +1,44 @@
 import requests
 
-import beautifulsoup
 from bs4 import BeautifulSoup
 
-#CTV links
-ctvURL = "https://www.ctvnews.ca/world/masked-assailants-ransack-venezuela-opposition-s-headquarters-as-post-election-tensions-mount-1.6987204"
+# parse into html reader
+URL = "https://www.cnn.com/2024/08/02/politics/plea-deal-9-11-khalid-sheikh-mohammed-revoked-austin/index.html"
 page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
 
-soup = BeautifulSoup(html, "html.parser")
-
-#CTV, CNN
-title = soup.find("title")
-
+# search title (all news outlets)
+title = soup.find("title").gettext()
 print(title)
 
-ctvDescription = soup.find_all("div", class_="c-text")
 
+# CTV body description only 
+description = soup.find_all("div", {'class':"c-text"})
+
+for div in description:
+    paragraphs = div.find_all('p')
+    for paragraph in paragraphs:
+        print('---')
+        print(paragraph.get_text())
+        print('---')
 for line in description:
     oneLine = line.find('p').get_text()
     print(oneLine)
+    
+# CNN, FOX body description only
+description = soup.find('script', type='application/ld+json')
 
+if description:
+    json_content = description.string
+    data = json.loads(json_content)
 
-#CNN BODY PARAGRAPH 
-cnnURL = "https://www.cnn.com/2024/08/02/politics/plea-deal-9-11-khalid-sheikh-mohammed-revoked-austin/index.html"
-cnnDescription = soup.find_all(attrs={'articleBody': True})
+    # Step 5: Get the articleBody value
+    article_body = data.get('articleBody', 'No articleBody found')
 
-for bodyParagraph in cnnDescription:
-   print(bodyParagraph)
+    # Print the articleBody
+    print(article_body)
+else:
+    print('No JSON-LD script found')
 
 
 
